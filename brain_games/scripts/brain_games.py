5b27151f96a387d2121843470b90e61b170504a7
NadejdaShashina <nadejdashashina@gmail.com>
2023-03-10 17:42:54 +0400

    commit all

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -6,8 +6,8 @@ def greet():
     print("Welcome to the Brain Games!")
 
 def main():
-    //greet()
-    welcome_user()
+    #greet()
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
