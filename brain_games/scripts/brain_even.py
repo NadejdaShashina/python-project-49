b18b1601f369960273f7b3b6164afb1a3bb80d2f
NadejdaShashina <nadejdashashina@gmail.com>
2023-08-22 18:57:37 +0400

    add fiiles

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,26 +6,28 @@ def welcome_user():
     name = prompt.string('May I have your name? ')
     print("Hello,"+name)
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    sym1 = random.randint()
-    print("Question:", sym1)
-    your_answer = prompt.string('Your answer: ')
     i=0
     while i<3:
-        if (sym1 % 2 == 0) and (your_answer == 'no'):
-            print("'", your_answer, "' is wrong answer ;(. Correct answer was 'yes'.")
-            print("Let's try again, Bill!")
+        sym1 = random.randint(0, 100)
+        print("Question:", sym1)
+        your_answer = prompt.string('Your answer: ')
+        if (sym1 % 2 == 0) and (your_answer != 'yes'):
+            print("'", your_answer, "' is wrong answer ;(. Correct answer was 'yes'.", sep='')
+            print("Let's try again, "+name+"!")
             break
-        elif (sym1 % 2 != 0) and (your_answer == 'yes'):
-            print("'", your_answer, "' is wrong answer ;(. Correct answer was 'no'.")
-            print("Let's try again, Bill!")
+        elif (sym1 % 2 != 0) and (your_answer != 'no'):
+            print("'", your_answer, "' is wrong answer ;(. Correct answer was 'no'.", sep='')
+            print("Let's try again, "+name+"!")
             break
         else:
             print("Correct!")
+            if i == 2:
+                print("Congratulations, ", name, "!", sep='')
+        i += 1
 
-        print("Congratulations, ",  name, "!", sep='')
+
 def main():
 
-    # greet()
     welcome_user()
 
 if __name__ == '__main__':
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,43 @@
+import prompt, random, math
+
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+
+
+def wrong_answer(name, your_answer, true_answer):
+    print("'", your_answer, "' is wrong answer ;(. Correct answer was '", true_answer, "'.", sep='')
+    print("Let's try again, " + name + "!")
+
+
+def true_answer_message():
+    print("Correct!")
+
+
+def main():
+    welcome_user()
+    name = prompt.string('May I have your name? ')
+    print("Hello," + name)
+    print("Find the greatest common divisor of given numbers.")
+    i = 0
+
+    while i < 3:
+        num1 = random.randint(1, 99)
+        num2 = random.randint(1, 99)
+        true_answer = math.gcd(num1, num2)
+        question = str(num1) + " " + str(num2)
+        print("Question:", question)
+
+        your_answer = int(prompt.string('Your answer: '))
+        if your_answer == true_answer:
+            true_answer_message()
+            if i == 2:
+                print("Congratulations, ", name, "!", sep='')
+        else:
+            wrong_answer(name, your_answer, true_answer)
+            break
+        i += 1
+
+
+if __name__ == '__main__':
+    main()
